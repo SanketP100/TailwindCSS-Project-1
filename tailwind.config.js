857c31c61589503a5c25ec71276d40162baae0ec
sanket <sankrtpowar@gmail.com>
2024-06-07 03:37:58 -0700

    My first project in Tailw indCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ArchivoBlack:["Archivo Black", 'sans-serif'],
+        Roboto:["Roboto",'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
